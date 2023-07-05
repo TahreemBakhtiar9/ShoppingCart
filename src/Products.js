3072744bb0ec5ef1ba44c1f9aa434ec2833ce898
TahreemBakhtiar9 <tehreembakhtiar927@gmail.com>
2023-07-05 16:20:43 +0500

    basic layout

---
+++ src/Products.js
@@ -0,0 +1,9 @@
+
+export const PRODUCTS = [
+    (
+        id: 1,
+        productName: 'Bread',
+        price: 65.0,
+        productImage: , 
+    )
+]
---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+        <div className='shopName'>
+            <h1> Shopping Cart</h1>
+        </div>
+        <div className='products'>
+
+        </div>
+
+    </div>
+  )
+}
